dd50b76a672351cec5897d86617736974445f936
Arthur <arthur.richards@bristol.ac.uk>
2019-10-09 22:14:23 +0100

    Revised fgplot to enable import as library

--- fgplot.py
+++ fgplot.py
@@ -2,37 +2,47 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 
-if len(sys.argv)!=2:
-  print('{} logfile'.format(sys.argv[0]))
-  exit()
+def import_log(file_name):
 
-data_dict = {}
+  print('Opening {}'.format(file_name))
+  data_dict = {}
 
-with open(sys.argv[1]) as csv_file:
+  with open(file_name) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        line_count += 1
-        #print(f'\t{row[0]} {row[1]} {row[2]}')
-        if not row[1] in data_dict.keys():
-            data_dict[row[1]] = []
-        try:
-            data_dict[row[1]].append((float(row[0]),float(row[2])))
-        except ValueError:
-            pass
+      line_count += 1
+      if not row[1] in data_dict.keys():
+        data_dict[row[1]] = ([],[])
+      value = None
+      stamp = None
+      try:
+        value = float(row[2])
+        stamp = float(row[0])
+      except ValueError:
+        pass
+      if value and stamp:
+        data_dict[row[1]][0].append(stamp)
+        data_dict[row[1]][1].append(value)
     print('Processed {} lines.'.format(line_count))
-    #print(data_dict)
+  return(data_dict)
 
-plot_keys = [k for k in data_dict if len(data_dict[k])>1]
-print(plot_keys)
+if __name__=='__main__':
+  if len(sys.argv)!=2:
+    print('{} logfile'.format(sys.argv[0]))
+    exit()
 
-for i,key in enumerate(plot_keys):
-  if i==0:
-    ax=plt.subplot(len(plot_keys),1,i+1)
-    top_ax = ax
-  else:
-    ax=plt.subplot(len(plot_keys),1,i+1,sharex=top_ax)
-  plt.plot([d[0] for d in data_dict[key]],[d[1] for d in data_dict[key]])
-  plt.title(key)
+  data_dict = import_log(sys.argv[1])
 
-plt.show()
+  plot_keys = [k for k in data_dict if len(data_dict[k][0])>1]
+  print(plot_keys)
+
+  for i,key in enumerate(plot_keys):
+      if i==0:
+        ax=plt.subplot(len(plot_keys),1,i+1)
+        top_ax = ax
+      else:
+        ax=plt.subplot(len(plot_keys),1,i+1,sharex=top_ax)
+      plt.plot(data_dict[key][0],data_dict[key][1])
+      plt.title(key)
+  plt.show()
