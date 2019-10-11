b09da1c0f9442a316e3b3d5c8a33cab793d1c5c8
Arthur <arthur.richards@bristol.ac.uk>
2019-10-11 15:45:43 +0100

    Making sols PID demo easier to adjust

--- .sols/hdg_pid.py
+++ .sols/hdg_pid.py
@@ -19,14 +19,14 @@ while True:
   hdg = c.heading_deg()
   # P
   err = hdg_des - hdg
-  ail = 0.005*err
+  ail = 0.005*1.0*err
   # D
   if last_hdg:
-    ail += -0.03*(hdg - last_hdg)/0.5
+    ail += -0.03*1.0*(hdg - last_hdg)/0.5
   last_hdg = hdg
   # I
   int_err += 0.5*err
-  ail += 0.001*int_err
+  ail += 0.001*1.0*int_err
   # out
   c.set_aileron(ail)
   print(hdg_des,hdg,ail)
