307a075bb5c4a96cd103fd00d5b30963f14760ab
Arthur Richards <aeagr@bristol.ac.uk>
2020-10-08 09:38:54 +0100

    Tidying up

--- fgplot.py
+++ fgplot.py
@@ -5,56 +5,54 @@ import matplotlib.pyplot as plt
 
 def import_log(file_name):
 
-  print('Opening {}'.format(file_name))
-  data_dict = {}
+    print('Opening {}'.format(file_name))
+    data_dict = {}
 
-  with open(file_name) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-      line_count += 1
-      if not row[1] in data_dict.keys():
-        data_dict[row[1]] = ([],[])
-      value = None
-      stamp = None
-      try:
-        value = float(row[2])
-        stamp = float(row[0])
-      except ValueError:
-        pass
-      if value and stamp:
-        data_dict[row[1]][0].append(stamp)
-        data_dict[row[1]][1].append(value)
-    print('Processed {} lines.'.format(line_count))
-  return(data_dict)
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            line_count += 1
+            if not row[1] in data_dict.keys():
+                data_dict[row[1]] = ([], [])
+                value = None
+                stamp = None
+            try:
+                value = float(row[2])
+                stamp = float(row[0])
+            except ValueError:
+                pass
+            if value and stamp:
+                data_dict[row[1]][0].append(stamp)
+                data_dict[row[1]][1].append(value)
+        print('Processed {} lines.'.format(line_count))
+    return(data_dict)
 
 def plot_log(file_name):
 
-  data_dict = import_log(file_name)
+    data_dict = import_log(file_name)
 
-  plot_keys = [k for k in data_dict if len(data_dict[k][0])>1]
-  print(plot_keys)
+    plot_keys = [k for k in data_dict if len(data_dict[k][0]) > 1]
+    print(plot_keys)
 
-  for i,key in enumerate(plot_keys):
-      if i==0:
-        ax=plt.subplot(len(plot_keys),1,i+1)
-        top_ax = ax
-      else:
-        ax=plt.subplot(len(plot_keys),1,i+1,sharex=top_ax)
-      plt.plot(data_dict[key][0],data_dict[key][1])
-      plt.title(key)
-  plt.show()
+    for i, key in enumerate(plot_keys):
+        if i == 0:
+            this_ax = plt.subplot(len(plot_keys), 1, i+1)
+            top_ax = this_ax
+        else:
+            this_ax = plt.subplot(len(plot_keys), 1, i+1, sharex=top_ax)
+        plt.plot(data_dict[key][0], data_dict[key][1])
+        plt.title(key)
+    plt.show()
 
-if __name__=='__main__':
-    if len(sys.argv)==2:
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
         file_name = sys.argv[1]
     else:
         file_list = [fn for fn in os.listdir('logs') if fn.endswith('.csv')]
-        if len(file_list)==0:
+        if len(file_list) == 0:
             print('Unable to find log file')
             sys.exit(1)
         else:
             file_name = 'logs/'+file_list[-1]
     plot_log(file_name)
-
-
--- vs_p.py
+++ vs_p.py
@@ -4,13 +4,13 @@ c.ap_pitch_off()
 
 kk = 0
 while True:
-  kk+=1
-  c.tic()
-  if kk>10:
-    vs_des = 5.0
-  else:
-    vs_des = 0.0
-  vs = c.vertical_speed_fps()
-  c.set_elevator(-0.01*(vs_des - vs))
-  print(vs)
-  c.toc(0.5)
+    kk += 1
+    c.tic()
+    if kk > 10:
+        vs_des = 5.0
+    else:
+        vs_des = 0.0
+    vs = c.vertical_speed_fps()
+    c.set_elevator(-0.01*(vs_des - vs))
+    print(vs)
+    c.toc(0.5)
