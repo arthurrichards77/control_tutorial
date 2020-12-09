cb71d6060a4f3f5143001d6a55b985e86752b693
Arthur Richards <aeagr@bristol.ac.uk>
2020-12-09 15:12:03 +0000

    Modeling example now leads to lag compensator, with corresponding response example.

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 """
 Fit linear system model to step response data
+then design simple lag compensator
 """
 from fgplot import import_log, last_log_filename
 import matplotlib.pyplot as plt
-from numpy import argmax, argmin, array, dot, real, imag, linspace
-from numpy.linalg import eig
+import numpy as np
+import scipy.optimize
+import scipy.signal
 from math import log, sqrt, pi
 
 # import last log data
@@ -23,11 +25,11 @@ plt.show()
 
 # extract peak information
 y_max = max(v) # first peak in output
-k_ymax = argmax(v) # and its index in the data
+k_ymax = np.argmax(v) # and its index in the data
 y_tr1 = min(v[k_ymax:-1]) # first trough after max
-k_ytr1 = k_ymax+argmin(v[k_ymax:-1]) # and its index in the data
+k_ytr1 = k_ymax+np.argmin(v[k_ymax:-1]) # and its index in the data
 y_pk2 = max(v[k_ytr1:-1]) # second peak
-k_ypk2 = k_ytr1+argmax(v[k_ytr1:-1]) # and its index in the data
+k_ypk2 = k_ytr1+np.argmax(v[k_ytr1:-1]) # and its index in the data
 
 # extract steady state values
 u_ss = min(e) # the size of the input step
@@ -47,7 +49,7 @@ plt.plot(te,e)
 plt.plot([te[0],te[-1]],[u_ss,u_ss],'k:')
 plt.show()
 
-# now use the log decrement method to find the system model
+# %% now use the log decrement method to find the system model
 # https://en.wikipedia.org/wiki/Logarithmic_decrement
 logdec = log((y_max-y_ss)/(y_pk2-y_ss)) # log ratio of peak heights
 zeta = 1/sqrt(1+(2*pi/logdec)**2) # gives us damping ratio
@@ -55,81 +57,92 @@ wd = 2*pi/(tv[k_ypk2] - tv[k_ymax]) # time between peaks gives damped natural fr
 wn = wd/sqrt(1-zeta**2) # undamped natural freq
 k_ss = y_ss/u_ss # scalar gain
 
-A = array([[0,1],[-wn*wn,-2*zeta*wn]])
-B = array([[0.0],[wn*wn]])*k_ss 
-C = array([[1.0,0]]) 
-D = array([0.0])
+A = np.array([[0,1],[-wn*wn,-2*zeta*wn]])
+B = np.array([[0.0],[wn*wn]])*k_ss 
+C = np.array([[1.0,0]]) 
+D = np.array([0.0])
 
 # make data set comparable - remove offset and time before step
-t_data = [t - tv[0] - 5.0 for t in tv if t > tv[0]+5]
-y_data = array([v[k] for k in range(len(v)) if tv[k] > tv[0]+5])
+t_data = [t - tv[0] - 5.0 for t in tv if t >= tv[0]+5]
+y_data = np.array([v[k] for k in range(len(v)) if tv[k] >= tv[0]+5])
 
-import scipy.signal
 (t_mdl,y_mdl) = scipy.signal.step((A,B,C,D),T=t_data)
 plt.figure()
 plt.plot(t_mdl,u_ss*y_mdl,'k')
 plt.plot(t_data,y_data) # compare with real data
 plt.show()
 
-# use more formal approach - optimize the fit
+# %% use more formal approach - optimize the fit
 # first define mismatch cost
 def sys_id_cost(p):
-    A2 = A + array([[0,0],[p[0],p[1]]])
-    B2 = B + array([[0.0],[p[2]]])
+    A2 = A + np.array([[0,0],[p[0],p[1]]])
+    B2 = B + np.array([[p[2]],[p[3]]])
     (t_mdl,y_mdl) = scipy.signal.step((A2,B2,C,D),T=t_data)
     errors = u_ss*y_mdl - y_data
     #print(errors)
     return sum(errors*errors)
 
 # test it with initial guess
-p0 = [0.0, 0.0, 0.0]
+p0 = [0.0, 0.0, 0.0, 0.0]
 print('Initial error cost:',
       sys_id_cost(p0),
       'Starting optimizer.')
 
 # now minimize this cost
-import scipy.optimize
-res = scipy.optimize.minimize(sys_id_cost,p0)
+res = scipy.optimize.minimize(sys_id_cost,p0,
+                              method='BFGS')
 print(res)
 
 # let's see what it gave us
 p = res.x
-A = A + array([[0,0],[p[0],p[1]]])
-B = B + array([0.0,[p[2]]])
-C = array([[1.0,0]])
-D = array([0.0])
-(t_mdl,y_mdl) = scipy.signal.step((A,B,C,D),T=t_data)
+A3 = A + np.array([[0,0],[p[0],p[1]]])
+B3 = B + np.array([[p[2]],[p[3]]])
+C = np.array([[1.0,0]])
+D = np.array([0.0])
+(t_mdl,y_mdl) = scipy.signal.step((A3,B3,C,D),T=t_data)
 plt.figure()
 plt.plot(t_mdl,u_ss*y_mdl,'k')
 plt.plot(t_data,y_data) # compare with real data
 plt.show()
 
-# plot the root locus
+# %% draw the Bode diagram
+w, mag, phase = scipy.signal.bode((A3,-B3,C,D))
 plt.figure()
-for kp in linspace(0,0.05):
-    Acl = A+kp*dot(B,C)
-    w,v = eig(Acl)
-    plt.plot(real(w),imag(w),'x')
+plt.subplot(2,1,1)
+plt.grid()
+plt.semilogx(w, mag)    # Bode magnitude plot
+plt.subplot(2,1,2)
+plt.grid()
+plt.semilogx(w, phase)  # Bode phase plot
 plt.show()
 
-# try to place both poles together on the real axis
-# do this with a bisection search
-k_ub = 0.05
-k_lb = 0.001
-for kk in range(20):
-    kp = 0.5*(k_ub+k_lb)
-    Acl = A+kp*dot(B,C)
-    w,v = eig(Acl)
-    #print(k_lb,k_ub,w)
-    if any(imag(w)):
-        k_lb = kp
-    else:
-        k_ub = kp
-print(kp,w)
-
-# finally have a look at how this should respond to a 5FPS step
-Bcl = -5*kp*B
-(t_pred,y_pred) = scipy.signal.step((Acl,Bcl,C,D))
+# %% add a scalar gain of x0.005
+w, mag, phase = scipy.signal.bode((A3,-0.005*B3,C,D))
 plt.figure()
-plt.plot(t_pred,y_pred) # compare with real data
+plt.subplot(2,1,1)
+plt.grid()
+plt.semilogx(w, mag)    # Bode magnitude plot
+plt.subplot(2,1,2)
+plt.grid()
+plt.semilogx(w, phase)  # Bode phase plot
 plt.show()
+
+# %% now add lag compensator - boost gain at low frequencies
+lag_comp = scipy.signal.TransferFunction([1,0.1],[1,0.01])
+openloop_sys = lag_comp.to_ss()*scipy.signal.StateSpace(A3,-0.005*B3,C,D)
+w, mag, phase = scipy.signal.bode(openloop_sys)
+plt.figure()
+plt.subplot(2,1,1)
+plt.grid()
+plt.semilogx(w, mag)    # Bode magnitude plot
+plt.subplot(2,1,2)
+plt.grid()
+plt.semilogx(w, phase)  # Bode phase plot
+plt.show()
+
+# %% get discrete form of compensator
+lag_disc = lag_comp.to_ss().to_discrete(dt=0.5)
+print('comp_a =',lag_disc.A[0,0])
+print('comp_b =',lag_disc.B[0,0])
+print('comp_c =',lag_disc.C[0,0])
+print('comp_d =',lag_disc.D[0,0])
---
+++ vs_comp.py
@@ -0,0 +1,30 @@
+from fgclient import FgClient
+c = FgClient()
+c.ap_pitch_off()
+
+# compensator definition
+comp_a = 0.9950124791926824
+comp_b = 0.4987520807317687
+comp_c = 0.09000000000000001
+comp_d = 1.0
+x_comp = 0.0 # initial internal state
+
+kk = 0
+dt = 0.5
+while True:
+    kk += 1
+    c.tic()
+    if kk > 10:
+        vs_des = 5.0
+    else:
+        vs_des = 0.0
+    vs = c.vertical_speed_fps()
+    err = vs_des - vs
+    # compensator
+    y_comp = comp_c*x_comp + comp_d*err
+    x_comp = comp_a*x_comp + comp_b*err
+    # apply feedback
+    #c.set_elevator(-0.005*y_comp) # with compensator
+    c.set_elevator(-0.005*err) # without compensator
+    print(vs)
+    c.toc(dt)
