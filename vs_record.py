90ac5ae3e26ab2b33d5b296d4a231a83500354e1
Arthur Richards <aeagr@bristol.ac.uk>
2020-12-09 13:45:32 +0000

    Improved step response recording

--- vs_record.py
+++ vs_record.py
@@ -1,10 +1,27 @@
+"""
+Execute step input on elevator and record step response in log
+"""
 from fgclient import FgClient
 c = FgClient()
+
+# require level flight to begin
+vs = c.vertical_speed_fps()
+if abs(vs) > 0.02:
+    raise ValueError('Vertical speed too large: ', vs)
+
 c.ap_pitch_off()
 
-while True:
-  c.tic()
-  vs = c.vertical_speed_fps()
-  e = c.get_elevator()
-  print(e,vs)
-  c.toc(0.5)
+time_step = 0.5
+for kk in range(120):
+    c.tic()
+    vs = c.vertical_speed_fps()
+    if kk < 10:
+        e = 0.0
+    else:
+        e = -0.02
+    c.set_elevator(e)
+    print(kk, e, vs)
+    c.toc(0.5)
+
+c.set_elevator(0.0)
+c.ap_pitch_vs(0.0)
